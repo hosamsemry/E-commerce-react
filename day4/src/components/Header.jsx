5f233c838996a5cc3faefe642f836150e11344a1
hosam <semryhosam@gmail.com>
2025-02-16 17:03:29 +0200

    made the header sticky and added a products count

--- day4/src/components/Header.jsx
+++ day4/src/components/Header.jsx
@@ -3,13 +3,19 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { NavLink } from 'react-router-dom';
-
-
+import { TiShoppingCart } from "react-icons/ti";
+import "../styles/main.css";
+import { useSelector } from 'react-redux';
 
 
 export default function Header() {
+
+  const cart = useSelector((store) => store.cartReducer.cart);
+  const count = cart.length;
+
+
   return (
-    <div>
+    <div className='fixed-top'>
       <Navbar bg="dark" className='ps-3' data-bs-theme="dark">
         
           <Navbar.Brand href="#home" style={{color:"lightgreen"}}>The Green Basket Shop</Navbar.Brand>
@@ -17,6 +23,7 @@ export default function Header() {
             <NavLink className={({ isActive }) => isActive ? "text-success nav-link" : "nav-link text-white"} to="/">Home</NavLink>
             <NavLink className={({ isActive }) => isActive ? "text-success nav-link" : "nav-link text-white"} to="/products">Products</NavLink>
             <NavLink className={({ isActive }) => isActive ? "text-success nav-link" : "nav-link text-white"} to="/about">About US</NavLink>
+            <NavLink className={"shopping-cart d-flex justify-content-between ms-1"} to="/checkout"> <TiShoppingCart /> <span className='text-decoration-none'>({count})</span></NavLink>
           </Nav>
         
       </Navbar>
