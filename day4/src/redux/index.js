68ad93c36f690072552ec8630b06d222425826c4
hosam <semryhosam@gmail.com>
2025-02-16 17:01:55 +0200

    deleted unnecessary files

--- day4/src/redux/index.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { productReducer } from "./productSlice";
-
-export const myStore = configureStore({
-	reducer: {
-		productSlice: productReducer,
-	},
-});
