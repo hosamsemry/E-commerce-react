4d308c24daada31f5b32d2c66a6aea775c188c4c
hosam <semryhosam@gmail.com>
2025-02-19 19:39:26 +0200

    added search for home page

--- day4/src/pages/Home.jsx
+++ day4/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { FaStar } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import { FaSearch } from "react-icons/fa";
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllProductsAction } from '../store/productSlice.js';
 import { addToCartAction } from '../store/cartSlice.js';
@@ -13,6 +14,15 @@ export default function Home() {
   const {products,isLoading,errors} = useSelector((store)=> store.productSlice)
   const dispatch = useDispatch();
   const [user, setUser] = React.useState(JSON.parse(localStorage.getItem('user')));
+  const [searchTerm, setSearchTerm] = useState("");
+  
+  const handleSearch = (event) => {
+      setSearchTerm(event.target.value);
+    };
+  
+  const filteredProducts = products.filter(product =>
+      product.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
   
     useEffect(() => {
       const handleStorageChange = () => {
@@ -41,8 +51,71 @@ export default function Home() {
 
   return (
     <>
-    
+    {/* <input type="text" 
+            placeholder='Search for products' 
+            className='col-8 offset-2 search' 
+            value={searchTerm}
+            onChange={handleSearch}/>  */}
+            <form className="d-flex search-box col-8 offset-2">
+                    <div className="input-group">
+                        <input type="search" className="form-control " value={searchTerm}
+                          onChange={handleSearch} placeholder="Search for items..."/>
+                        
+                        <button className="btn btn-success" type="submit">
+                        <FaSearch />
+                        </button>
+                    </div>
+                </form>
+
     <div className='container my-5 bg-light p-5'>
+
+    <h1 className='text-center mb-5 fw-bold bg-info p-2 text-light'>Our Products</h1>
+      <div className="row">
+          {filteredProducts.map((product) => (
+            <Card className='mb-4 mx-2 p-3' style={{ width: '25rem' }} key={product.id}>
+                <Card.Img variant="top" src={product.imgUrl} className='img-fluid' style={{height:"250px"}}/>
+                <Card.Body>
+                    <Card.Title><div className='fs-3 mb-3 fw-bold'>{product.name.charAt(0).toUpperCase() + product.name.slice(1).toLowerCase()}</div> </Card.Title>
+                    <Card.Text>
+                    <span className='fw-bold fs-5 me-1 '>Price:</span> {product.price} <span style={{color:'darkgreen'}}>$</span>
+                    </Card.Text>
+                    <Card.Text>
+                    <span className='fw-bold fs-5 me-1 '>Category:</span> {product.category.charAt(0).toUpperCase() + product.category.slice(1).toLowerCase()}
+                    </Card.Text>
+                    <Card.Text>
+                    <span className='fw-bold fs-5 me-1 '>Available Quantity:</span> {product.quantity}
+                    </Card.Text>
+                    <Card.Text>
+                     
+                     <FaStar className='text-warning fs-5 '/>
+                     <FaStar className='text-warning fs-5 '/>
+                     <FaStar className='text-warning fs-5 '/>
+                     <FaStar className='text-warning fs-5 '/>
+                     <FaStar className='text-warning fs-5 '/>
+                    </Card.Text>
+
+                    <div className='d-flex justify-content-between'>
+                    
+                    {user &&
+                    <Button  variant="primary" className='btn btn-success fw-bold' onClick={()=>{
+                        dispatch(addToCartAction(product));
+                    }}>Add To Cart</Button>
+                    }
+                    
+                    
+                    <Link to={`/products/${product.id}`}>
+                    <Button  variant="primary" className='btn btn-warning fw-bold'>Product Details</Button>
+                    </Link>
+
+                    </div>
+
+                </Card.Body>
+            </Card>
+            ))}
+          
+            
+        </div>
+
       <h1 className='text-center mb-5 fw-bold bg-success p-2 text-light'>Best seller</h1>
       <div className="row mb-5">
           {products.filter(product => product.price < 15).map((product) => (
@@ -90,52 +163,7 @@ export default function Home() {
             
         </div>
 
-      <h1 className='text-center mb-5 fw-bold bg-info p-2 text-light'>Our Products</h1>
-      <div className="row">
-          {products.map((product) => (
-            <Card className='mb-4 mx-2 p-3' style={{ width: '25rem' }} key={product.id}>
-                <Card.Img variant="top" src={product.imgUrl} className='img-fluid' style={{height:"250px"}}/>
-                <Card.Body>
-                    <Card.Title><div className='fs-3 mb-3 fw-bold'>{product.name.charAt(0).toUpperCase() + product.name.slice(1).toLowerCase()}</div> </Card.Title>
-                    <Card.Text>
-                    <span className='fw-bold fs-5 me-1 '>Price:</span> {product.price} <span style={{color:'darkgreen'}}>$</span>
-                    </Card.Text>
-                    <Card.Text>
-                    <span className='fw-bold fs-5 me-1 '>Category:</span> {product.category.charAt(0).toUpperCase() + product.category.slice(1).toLowerCase()}
-                    </Card.Text>
-                    <Card.Text>
-                    <span className='fw-bold fs-5 me-1 '>Available Quantity:</span> {product.quantity}
-                    </Card.Text>
-                    <Card.Text>
-                     
-                     <FaStar className='text-warning fs-5 '/>
-                     <FaStar className='text-warning fs-5 '/>
-                     <FaStar className='text-warning fs-5 '/>
-                     <FaStar className='text-warning fs-5 '/>
-                     <FaStar className='text-warning fs-5 '/>
-                    </Card.Text>
-
-                    <div className='d-flex justify-content-between'>
-                    
-                    {user &&
-                    <Button  variant="primary" className='btn btn-success fw-bold' onClick={()=>{
-                        dispatch(addToCartAction(product));
-                    }}>Add To Cart</Button>
-                    }
-                    
-                    
-                    <Link to={`/products/${product.id}`}>
-                    <Button  variant="primary" className='btn btn-warning fw-bold'>Product Details</Button>
-                    </Link>
-
-                    </div>
-
-                </Card.Body>
-            </Card>
-            ))}
-          
-            
-        </div>
+      
     </div>
     </>
   )
